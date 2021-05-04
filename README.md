4de5e5daff054ec0f421d0ffe541e907e1bc8e1d
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2021-05-04 18:40:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# svg-to-png
+ It allows you to easly convert svg vectors to png images!
---
+++ script.js
@@ -0,0 +1,119 @@
+Vue.use(VueMaterial.default);
+
+Vue.config.errorHandler = (err, vm, info) => {
+  if (err.message !== "Cannot read property 'badInput' of undefined") {
+    console.error(err);
+  }
+};
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+const app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      inType: "file",
+      inFile: null,
+      inURL: "",
+      svgURL: "",
+      customWidth: 0,
+      customHeight: 0
+    }
+  },
+  methods: {
+    async generateAndDownload() {
+      let img = await getImage(this.svgURL);
+      canvas.width = this.customWidth;
+      canvas.height = this.customHeight;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      let dataURL = canvas.toDataURL();
+      saveAs(dataURL, `armagan-svg-to-png-${Date.now()}.png`);
+    },
+    resetOptions() {
+      this.customWidth = 0;
+      this.customHeight = 0;
+      this.svgURL = "";
+    },
+    async onInUpdate() {
+      switch (this.inType) {
+        case "file": {
+          let file = document.getElementById(this.$refs.file.id)?.files?.[0];
+          if (!file) return this.resetOptions();
+          let dataURL = await readFile(file, "DataURL");
+          this.svgURL = dataURL;
+          break;
+        };
+        case "url": {
+          if (!this.inURL) return this.resetOptions();
+          this.svgURL = this.inURL;
+          break;
+        };
+      };
+
+      try {
+        let img = await getImage(this.svgURL);
+        this.customWidth = img.width;
+        this.customHeight = img.height;
+        img = 0;
+        if (
+          this.customWidth == 0 ||
+          this.customHeight == 0
+        ) return this.resetOptions();
+      } catch {
+        return this.resetOptions();
+      }
+
+      
+    }
+  },
+  watch: {
+    inType(...args) { this.onInUpdate(...args) },
+    inFile(...args) { this.onInUpdate(...args) },
+    inURL(...args) { this.onInUpdate(...args) }
+  }
+});
+
+/**
+ * @param {File} file 
+ * @param {"Text"|"ArrayBuffer"|"DataURL"|"BinaryString"} as 
+ * @param {String} encoding
+ * @returns 
+ */
+function readFile(file, as = "Text", encoding=null) {
+  return new Promise((res, rej) => {
+    let reader = new FileReader();
+    reader.onload = () => {
+      res(reader.result);
+      reader = 0;
+    };
+    reader.onerror = (err) => {
+      rej(err);
+      reader = 0;
+    };
+    reader[`readAs${as}`](file,encoding);
+  })
+}
+
+/**
+ * @param {String} src 
+ * @returns {HTMLImageElement}
+ */
+function getImage(src = "") {
+  return new Promise((res, rej) => {
+    let img = new Image();
+    if (src.toLowerCase().indexOf("http") != -1) {
+      img.addEventListener("load", () => {
+        res(img);
+      }, { once: true });
+    } else {
+      res(img);
+    }
+    
+    img.addEventListener("error", (err) => {
+      rej(err);
+      img = 0;
+    }, { once: true });
+    img.src = src;
+  })
+}
